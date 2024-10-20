26dcb83f0f8d7b595f51a1971f1a494ad4035f7e
Kaue Pilger <kauesouzapilger@gmail.com>
2024-10-20 11:07:52 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Feed } from "./pages/feed";
 import { Home } from './pages/home'
 import { Login } from './pages/login'
 import { GlobalStyle } from './styles/global';
+import { Register } from "./pages/register";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/feed" element={<Feed />} />
      </Routes >
     </Router>
